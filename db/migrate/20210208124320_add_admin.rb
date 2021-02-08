b7fd6f6ab8c4e37620f2741878c89e5978161b4a
Dmytro <zelenyid446@gmail.com>
2021-02-08 15:01:51 +0200

    Registration, authentication and roles with help gems devise and cancancan

---
+++ db/migrate/20210208124320_add_admin.rb
@@ -0,0 +1,9 @@
+class AddAdmin < ActiveRecord::Migration[6.1]
+  def change
+    User.create! do |u|
+      u.email     = 'test_admin@test.com'
+      u.password  = 'password'
+      u.admin = true
+    end
+  end
+end
